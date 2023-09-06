637d2cf473eacc095d0fe7c531ceef6e94232952
Nimaomu <nimaomuhyolmo@gmail.com>
2023-09-05 23:21:02 -0400

    initial commit

---
+++ src/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ",/header.css";
+
+function header() {
+  return (
+    <header className="Travel Destination">
+      <div className="logo"></div>
+      <nav>
+        <li>
+          <a href="#">Hiking Gear</a>
+        </li>
+        <li>
+          <a href="#">Navigation</a>
+        </li>
+        <li>
+          <a href="#">Camping Gear</a>
+        </li>
+      </nav>
+    </header>
+  );
+}
+
+export default header;
---
+++ src/maincontent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function maincontent() {
+  return <div>maincontent</div>;
+}
+
+export default maincontent;
